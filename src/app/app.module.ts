0cd65852e3fa71deba79229238474fb7a4da2045
Daftkiller <vivek.bharti@twinspark.co>
2018-09-19 23:32:28 +0530

    UI Changes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,14 @@ import {RouterModule} from '@angular/router';
 import { ROUTES } from './app.routes'
 import { AppComponent } from './app.component';
 import { Json2PdfComponent } from './components/json2-pdf/json2-pdf.component';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 
 import { HttpClientModule } from '@angular/common/http';
 import { PrettyPrintPipe } from './pretty-print.pipe';
 import { AlertModule } from 'ngx-bootstrap';
+import { ButtonsModule } from 'ngx-bootstrap';
+import {MatButtonModule} from '@angular/material/button';
+import {MatCardModule} from '@angular/material/card';
 
 @NgModule({
   declarations: [
@@ -17,8 +21,12 @@ import { AlertModule } from 'ngx-bootstrap';
   ],
   imports: [
     BrowserModule,
+    BrowserAnimationsModule,
+    MatButtonModule,
     HttpClientModule,
+    MatCardModule,
     AlertModule.forRoot(),
+    ButtonsModule.forRoot(),
     RouterModule.forRoot(ROUTES),
   ],
   providers: [PrettyPrintPipe],
